933f2392d19880119b6fa9af509ffb294ef1e1fe
xuchaoxi <xuchaoxikb@gmail.com>
2017-01-03 16:57:05 +0800

    modify sqlist.h

--- List/sqlist.h
+++ List/sqlist.h
@@ -13,15 +13,13 @@
 #define SIZE 100    //线性表初始存储分配量
 #define INCREMENT 10    //存储增量
 using namespace std;
+typedef int ElemType;
 typedef struct{
-    int *elem;      //存储空间基址
+    ElemType *elem;      //存储空间基址
     int length;     //当前长度
     int listsize;   //分配长度
 }SqList;
-//构造一个空的线性表
-int InitList_Sq(SqList &L);
-//销毁线性表
-int DestroyList(SqList &L);
+//构造一个空的线性表mZ
 //重置为空表
 int ClearList(SqList &L);
 //判断是否为空表
@@ -29,11 +27,21 @@ int ListEmpty(SqList L);
 //返回元素个数
 int ListLength(SqList L);
 //返回第i个元素
-int GetElem(SqList L, int i, int &e);
-
-
-
-
-
-
+int GetElem(SqList L, int i, ElemType &e);
+//元素定位
+int LocateElem(SqList L,ElemType e, int (*compare)(ElemType, ElemType));
+//前驱
+int PriorElem(SqList L, ElemType cur_e, ElemType &pre_e);
+//后继
+int NextElem(SqList L, ElemType cur_e, ElemType &next_e);
+//插入元素e
+int ListInsert(SqList &L, int i, ElemType e);
+//删除第i个元素
+int ListDelete(SqList &L, int i, ElemType &e);
+//遍历线性表
+int ListTraverse(SqList L, void (*visit)(ElemType *));
+//比较函数
+int compare(ElemType e1, ElemType e2);
+//输出函数
+void print(ElemType *e);
 
