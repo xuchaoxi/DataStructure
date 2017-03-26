7df42ca966aed944fcc80726d2cffd6ead3532fb
Chaoxi Xu <xuchaoxikb@gmail.com>
2017-03-26 11:32:46 +0800

    add SelectSort

--- Sort/basicSort.cpp
+++ Sort/basicSort.cpp
@@ -9,9 +9,11 @@
 using namespace std;
 
 /*
- * param data[], size
- * data!=NULL size > 0
- * data (<=)
+ * function : InsertSort
+ * param : data[], size
+ * con : data!=NULL size > 0
+ * con2 : data (<=)
+ * usage:
  * int arr[] = {10,9,8,4,5,7,6,3,1,4};
  * InsertSort(arr, 10);
  */
@@ -35,8 +37,11 @@ void InsertSort(T data[], int size)
 }
 
 /*
- * param data size cmp
- * data!=NULL size > 0
+ * function : InsertSort
+ * param : data size cmp
+ * con : data!=NULL size > 0
+ * con: data (f)
+ * usage:
  * bool cmp(int a, int b) { return a < b; }
  * int arr[] = {10,9,8,4,5,7,6,3,1,4};
  * InsertSort(arr, 10, cmp);
@@ -59,4 +64,79 @@ void InsertSort(T data[], int size, Func f)
         }
     }
 }
+
+
+/*
+ * function : SelectSort
+ * param : data size
+ * con : data!=NULL size>0
+ * con2 : data (<=)
+ * usage:
+ * #include<algorithm>
+ * int arr[] = {10,9,8,4,5,7,6,3,1,4};
+ * SelectSort(arr, 10);
+ */
+template <typename T>
+void SelectSort(T data[], int size)
+{
+    int i, j, k;
+    for(i = 0;i < size-1;++i)
+    {
+        k = i;
+        for(j = i+1;j < size;++j)  // SelectMin key from i+1 to size
+        {
+            if(data[j] < data[k])
+            {
+                k = j;
+            }
+        }
+        if(k != i)
+        {
+            std:swap(data[i], data[k]);
+        }
+    }
+}
+
+/*
+ * function : SelectSort
+ * param : data size cmp
+ * con : data!=NULL size>0
+ * con2 : data (f)
+ * usage:
+ * #include<algorithm>
+ * int arr[] = {10,9,8,4,5,7,6,3,1,4};
+ * bool cmp(int a, int b) { return a < b; }
+ * SelectSort(arr, 10);
+ */
+template <template T, template Func>
+void SelectSort(T data[], int size, Func f)
+{
+    int i, j, k;
+    for(i = 0;i < size-1;++i)
+    {
+        k = i;
+        for(j = i+1;j < size;++j)
+        {
+            if(f(data[j], data[k]))
+            {
+                k = j;
+            }
+        }
+        if(k != i)
+        {
+            std::swap(data[i], data[k]);
+        }
+    }
+}
+
+/*
+ * function : BubbleSort
+ * param : data[], size
+ * con : data!=NULL size > 0
+ * con2 : data (<=)
+ * usage:
+ * #include<algorithm>
+ * int arr[] = {10,9,8,4,5,7,6,3,1,4};
+ * BubbleSort(arr, 10);
+ */
 
